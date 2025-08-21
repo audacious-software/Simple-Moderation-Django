ec171482a87b78e7735544dc7e116fb9041b48a8
Chris J. Karr <chris@audacious-software.com>
2025-08-21 21:54:07 +0000

    Pulling Detoxify into its own package

--- moderation_api.py
+++ moderation_api.py
@@ -12,22 +12,6 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils import timezone
 
-from .models import ModerationDecision
-
-class NumpyEncoder(json.JSONEncoder):
-    """ Special json encoder for numpy types """
-    def default(self, obj): # pylint: disable=arguments-renamed
-        if isinstance(obj, numpy.integer):
-            return int(obj)
-
-        if isinstance(obj, numpy.floating):
-            return float(obj)
-
-        if isinstance(obj, numpy.ndarray):
-            return obj.tolist()
-
-        return json.JSONEncoder.default(self, obj)
-
 def moderate(request, moderator):
     logger = logging.getLogger()
 
@@ -71,28 +55,6 @@ def moderate(request, moderator):
             except ImportError:
                 # pass
                 logger.error('OK: %s', traceback.format_exc())
-
-        if moderator.moderator_id.startswith('detoxify:'):
-            from detoxify import Detoxify
-
-            model_name = moderator.moderator_id.replace('detoxify:', '')
-
-            results = Detoxify(model_name).predict(request.message)
-
-            logger.warning('simple_moderation.moderate.detoxify: %s -- %s -- %s', request, moderator, results)
-
-            decision = ModerationDecision(request=request, when=timezone.now())
-
-            if results['toxicity'] < settings.SIMPLE_MODERATION_DETOXIFY_THRESHOLD:
-                decision.approved = True
-            else:
-                decision.approved = False
-
-            decision.decision_maker = moderator.moderator_id
-
-            decision.metadata = json.dumps(results, indent=2, cls=NumpyEncoder)
-
-            decision.save()
     except: # pylint: disable=bare-except
         logger.error('simple_moderation.moderate ERROR: %s', traceback.format_exc())
 
